cf5a4ab091e8c907d16dbfc3165375ee3695e37e
JayWood <jjwood2004@gmail.com>
2023-05-28 18:01:21 -0400

    Finally able to override the entrypoint to run commands if need be.

--- config/cmd.sh
+++ config/cmd.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
+set -euo pipefail;
+
 echo "Entrypoint override"
-if mount | grep -q /var/www/html/wp-content
-then
+if mount | grep -q /var/www/html/wp-content; then
   # entrypoint from Wordpress initializes /var/www/html; ignores wp-content directory
   # If this is bind-mounted, it's a "development area".
-  echo "/var/www/html/wp-content is mounted, skipping"
+  echo "/var/www/html/wp-content is mounted, skipping";
 else
   # This is done because a volume is mapped on /var/www/html - otherwise on each startup the tree would get populated in an unnamed volume
   # resulting in many dangling volumes.
 
-  echo "Re-initializing /var/www/html ..."
-  rm -fr /var/www/html/*
+  echo "Re-initializing /var/www/html ...";
+  rm -fr /var/www/html/*;
 fi
 
-echo "Executing built-in entrypoint"
-exec /usr/local/bin/docker-entrypoint.sh "$@"
+echo "replacing file";
+sed -i 's#exec "$@"##' /usr/local/bin/docker-entrypoint.sh
+
+echo "Executing built-in entrypoint";
+. /usr/local/bin/docker-entrypoint.sh "$@";
+
+echo "Okay we're here now";
+
+exec "$@";
