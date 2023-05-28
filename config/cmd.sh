d7e793e6948ae75237446b0d99a98ff6e36ea90c
JayWood <jjwood2004@gmail.com>
2023-05-28 15:24:14 -0400

    Successful override

--- config/cmd.sh
+++ config/cmd.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-set -euox pipefail
+echo "Entrypoint override"
+if mount | grep -q /var/www/html/wp-content
+then
+  # entrypoint from Wordpress initializes /var/www/html; ignores wp-content directory
+  # If this is bind-mounted, it's a "development area".
+  echo "/var/www/html/wp-content is mounted, skipping"
+else
+  # This is done because a volume is mapped on /var/www/html - otherwise on each startup the tree would get populated in an unnamed volume
+  # resulting in many dangling volumes.
 
-echo "Made it here"
-#wp site list --allow-root
+  echo "Re-initializing /var/www/html ..."
+  rm -fr /var/www/html/*
+fi
 
-# Run the original foreground.
-apache2-foreground
-
-exec "$@"
+echo "Executing built-in entrypoint"
+exec /usr/local/bin/docker-entrypoint.sh "$@"
