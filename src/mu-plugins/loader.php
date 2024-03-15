9e496e56206cc6fa3e92d53ec0ca0f9a39d90f3e
JayWood <jjwood2004@gmail.com>
2024-03-14 21:21:19 -0400

    Update things

--- src/mu-plugins/loader.php
+++ src/mu-plugins/loader.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Plugin Name: mu-plugin Autoloader
- * Plugin URI: https://github.com/JayWood/
- * Description: This plugin autoloads plugins within the MU-Plugins directory.
- * Author: JayWood
- * Author URI: https://plugish.com/
- * Version: 1.0.0
- */
 $plugins = array(
 	'akismet/akismet.php'
 );
