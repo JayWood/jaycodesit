437fd624b198c30efe230ba5ef5275dc3a6fd96f
JayWood <jjwood2004@gmail.com>
2024-07-15 20:34:04 -0400

    update again maybe?

--- config/cmd.sh
+++ config/cmd.sh
@@ -21,6 +21,14 @@ sed -i 's#exec "$@"##' /usr/local/bin/docker-entrypoint.sh
 echo "Executing built-in entrypoint";
 . /usr/local/bin/docker-entrypoint.sh "$@";
 
+# Check if the database has tables
+if wp db tables --allow-root | grep -q "wp_"; then
+  echo "Database already has tables, skipping wp core install"
+else
+  echo "Database is empty, running wp core install"
+  wp core install --url="${WORDPRESS_URL}" --title="${WORDPRESS_TITLE}" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --skip-email --allow-root
+fi
+
 wp theme activate papanek --allow-root;
 
 exec "$@";
