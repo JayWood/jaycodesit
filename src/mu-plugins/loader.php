598c17ae93e6bd3ca22e7d903d4c3455b9a4b8d9
JayWood <jjwood2004@gmail.com>
2024-07-17 17:34:17 -0400

    remove auto updates

--- src/mu-plugins/loader.php
+++ src/mu-plugins/loader.php
@@ -8,3 +8,9 @@ foreach ( $plugins as $plugin ) {
 	wp_register_plugin_realpath( $path );
 	include $path;
 }
+
+add_filter( 'auto_update_plugin', '__return_false' );
+add_filter( 'auto_update_theme', '__return_false' );
+add_filter( 'allow_dev_auto_core_updates', '__return_false' );
+add_filter( 'allow_minor_auto_core_updates', '__return_false' );
+add_filter( 'allow_major_auto_core_updates', '__return_false' );
