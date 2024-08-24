eb7867118924ebfa88f88d872b681efcf21347e3
JayWood <jjwood2004@gmail.com>
2024-08-24 14:36:12 -0400

    Add some logs

--- src/mu-plugins/loader.php
+++ src/mu-plugins/loader.php
@@ -27,5 +27,8 @@ HTML;
 add_action('muplugins_loaded', function() {
 	if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
 		$_SERVER['HTTPS'] = 'on';
+		error_log( 'wtf' );
 	}
+
+	error_log( 'wtf2' );
 } );
